2723d5b80b7a88df864877971f10d8361ce47c0b
favoursamuel <favor.s@mblng.com>
2026-02-19 16:31:53 +0100

    fix for google search

--- includes/head.php
+++ includes/head.php
@@ -33,10 +33,10 @@
     <!-- Favicon -->
     <!-- <link rel="icon" type="image/png" sizes="32x32" href="assets/images/logo.png"> -->
      <!-- Favicon -->
-    <link rel="icon" href="assests/images/favicon.ico" sizes="any">
-    <link rel="icon" type="image/png" sizes="48x48" href="assests/images/favicon-48x48.png">
-    <link rel="icon" type="image/png" sizes="192x192" href="assests/images/favicon-192x192.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="assests/images/favicon-192x192.png">
+  <link rel="icon" href="/favicon.ico" sizes="any">
+  <link rel="icon" type="image/png" sizes="48x48" href="/favicon-48x48.png">
+  <link rel="icon" type="image/png" sizes="192x192" href="/favicon-192x192.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="/favicon-192x192.png">
 
     <link rel="apple-touch-icon" sizes="180x180" href="assets/images/logo.png">
 
