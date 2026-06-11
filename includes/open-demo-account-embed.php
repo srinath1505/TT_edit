96ae166559c001a20553da32a0b65a8cb7db52a8
Favor Samuel <favor.s@mblng.com>
2026-06-11 15:20:17 +0100

    embeded page

---
+++ includes/open-demo-account-embed.php
@@ -0,0 +1,5 @@
+<?php
+$embedPageTitle = 'Open Demo Account | TraderTok';
+include __DIR__ . '/partials/embed-form-document-start.php';
+include __DIR__ . '/partials/open-demo-account-embed-form.php';
+include __DIR__ . '/partials/embed-form-document-end.php';
